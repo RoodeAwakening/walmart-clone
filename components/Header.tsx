818c499eb035cd3f85394e728991c6287db70a33
Derek Roode <derek.i.roode@gmail.com>
2025-05-02 16:40:14 -0400

    feat: add utility functions, update header with logo, and enhance Tailwind configuration

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Image, Link } from "lucide-react";
+import React from "react";
 
 function Header() {
   return (
     <header>
-        
+      <Link href="/">
+        <Image
+          src="https://link/papareact.com/x9j"
+          alt="Walmart Logo"
+          width={150}
+          height={150}
+        />
+      </Link>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
