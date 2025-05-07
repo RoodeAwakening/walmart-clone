bbd6e7c0228a6a57a95fd3002793f489658e33ef
Derek Roode <derek.i.roode@gmail.com>
2025-05-07 15:43:41 -0400

    feat: update Product component layout and enhance title display with character limit

--- components/Product.tsx
+++ components/Product.tsx
@@ -7,7 +7,7 @@ import { Badge } from "./ui/badge";
 function Product({ product }: { product: Organic }) {
   return (
     <Link
-    className="flex flex-col relative border rounded-md h-full p-5 hover:shadow-lg"
+      className="flex flex-col relative border rounded-md h-full p-5 hover:shadow-lg"
       href={{
         pathname: "/product:",
         query: { url: product.url },
@@ -21,21 +21,25 @@ function Product({ product }: { product: Organic }) {
         className="mx-auto"
       />
       {product.price.price_strikethrough && (
-      <p className="text-sm text-gray-500 line-through">
-      ${product.price.price_strikethrough}
-      </p>
-        )}
-      <p className="text-xl font-bold">
-        {/* {product.general.title.length > 50
-          ? product.general.title.slice(0, 50) + "..."
-          : product.general.title} */}
-          
-          ${product.price.price}
-      </p>
+        <p className="text-sm text-gray-500 line-through">
+          ${product.price.price_strikethrough}
+        </p>
+      )}
+      <p className="text-xl font-bold">${product.price.price}</p>
 
       {product.general.badge && (
-        <Badge className="w-fit absolute top-2 right-2">{product.general.badge}</Badge>
+        <Badge className="w-fit absolute top-2 right-2">
+          {product.general.badge}
+        </Badge>
       )}
+
+      <p className="font-light">
+        {product.general.title.length > 70
+          ? product.general.title.slice(0, 70) + "..."
+          : product.general.title}{" "}
+      </p>
+
+      {product.rating && <p className="text-yellow-500 text-sm">{product.rating.rating} ★  <span className="text-gray-400 ml-2">{product.rating.count}</span></p>}
     </Link>
   );
 }
