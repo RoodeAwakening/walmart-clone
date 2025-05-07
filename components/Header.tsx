8d3d74fe79d1c844536086d297504c34afe8acc7
Derek Roode <derek.i.roode@gmail.com>
2025-05-07 12:04:26 -0400

    feat: enhance Header component layout and add navigation links

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { Search } from "lucide-react";
+import { Grid2X2, Heart, LayoutGrid, Search, User } from "lucide-react";
 
 function Header() {
   return (
-    <header className="flex bg-walmart">
-      <Link href="/">
+    <header className="flex flex-col md:flex-row bg-walmart px-10 py-7 space-x-5">
+      <Link href="/" className="mb-5 md:mb-0">
         <Image
           src="https://links.papareact.com/yur"
           alt="Walmart Logo"
@@ -16,11 +16,51 @@ function Header() {
         />
       </Link>
       <form className="flex items-center bg-white rounded-full w-full flex-1">
-        <input type="text" placeholder="Search Everything..." className="flex-1" />
-        <Button className="rounded-full h-10">
-          <Search />
+        <input
+          type="text"
+          placeholder="Search Everything..."
+          className="flex-1 px-4 rounded-l-full outline-none placeholder:text-sm"
+        />
+        <Button type="submit" className="rounded-full h-10 w-10 bg-yellow-400">
+          <Search className="text-black" />
         </Button>
       </form>
+      <div className="flex space-x-5 mt-5 md:mt-0 justify-evenly">
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Heart size={20} />
+          <div>
+            <p className="text-xs font-extralight">Reorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <User size={20} />
+          <div>
+            <p className="text-xs font-extralight">Sign In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
