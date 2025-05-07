4b61a88d3504af0338ce9d6e1d5e500ef234ea00
Derek Roode <derek.i.roode@gmail.com>
2025-05-07 15:14:03 -0400

    feat: enhance fetchSearch function to include revalidation and improve error handling

--- lib/fetchSearch.ts
+++ lib/fetchSearch.ts
@@ -1,34 +1,40 @@
 import { Result } from "@/typings/searchTypings";
 
 async function fetchSearch(searchTerm: string) {
-    const username = process.env.OXYLABS_USERNAME;
-    const password = process.env.OXYLABS_PASSWORD;
+  const username = process.env.OXYLABS_USERNAME;
+  const password = process.env.OXYLABS_PASSWORD;
 
-    const newUrl = new URL(`https://www.walmart.com/search?q=${searchTerm}`);
-    const body = {
-        source: 'universal_ecommerce',
-        url: newUrl.toString(),
-        country: 'United States',
-        parse: true,
+  const newUrl = new URL(`https://www.walmart.com/search?q=${searchTerm}`);
+  const body = {
+    source: "universal_ecommerce",
+    url: newUrl.toString(),
+    country: "United States",
+    parse: true,
+  };
+
+  const response = fetch("https://realtime.oxylabs.io/v1/queries", {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString(
+        "base64"
+      )}`,
+    },
+    next: {
+        revalidate: 60 * 60, // 1 hour
     }
-
-    const response = fetch("https://realtime.oxylabs.io/v1/queries", {
-        method: "post",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`,
-        },
-    }).then((res) => res.json())
-      .then((data) => {
-        if (data?.results.length === 0) return;
-        const result: Result = data.results[0];
-        return result
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data?.results.length === 0) return;
+      const result: Result = data.results[0];
+      return result;
     })
     .catch((error) => {
-        console.error("Error:", error);
-    })
-    return response;
+      console.error("Error:", error);
+    });
+  return response;
 }
 
 export default fetchSearch;
