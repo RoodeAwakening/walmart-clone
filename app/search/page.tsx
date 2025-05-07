88c5f9d0927703d9ac5b06a94836eedb3009c27c
Derek Roode <derek.i.roode@gmail.com>
2025-05-07 15:05:36 -0400

    feat: enhance SearchPage to display search results and update TypeScript typings

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,9 +8,21 @@ type Props = {
 
 async function SearchPage({ searchParams: { q } }: Props) {
   const results = await fetchSearch(q);
+  console.log('------------>', results?.content.results)
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Results For {q}</h1>
+      <h2>({results?.content?.page_details?.total_results} results)</h2>
 
-  console.log("results", results);
-  return <div>SearchPage</div>;
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {results?.content.results.map(product => (
+            <li key={product.general.product_id}>
+              {product.general.title}
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default SearchPage;
--- typings/searchTypings.ts
+++ typings/searchTypings.ts
@@ -1,101 +1,129 @@
 export interface SearchResult {
-    results: Result[];
-    total_results: number;
-    last_visible_page: number;
-    parse_status_code: number;
-    created_at: string;
-    status: string;
-    page: number;
-    url: string;
-    job_id: string;
-    status_code: number;
-    parse_type: string;
+  results: Result[];
+  total_results: number;
+  last_visible_page: number;
+  parse_status_code: number;
+  created_at: string;
+  status: string;
+  page: number;
+  url: string;
+  job_id: string;
+  status_code: number;
+  parse_type: string;
 }
 
 export interface Result {
-    content: Content;
+  content: Content;
 }
 
 export interface Content {
-    url: string;
-    organic: Organic[];
-    total_results: number;
-    last_visible_page: number;
-    pasre_status_code: number;
+  url: string;
+  organic: Organic[];
+  total_results: number;
+  last_visible_page: number;
+  parse_status_code: number;
+  page_details: PageDetails;
+  results: Organic[];
+}
+
+export interface PageDetails {
+  page: number;
+  total_results: number;
+  last_visible_page: number;
 }
 
 export interface Organic {
-    url: string;
-    image: string;
-    price: Price;
-    title: string;
-    rating: Rating;
-    seller: Seller;
-    product_id: string;
-    badge?: string;
-    variants?: Variant[];
+  url: string;
+  image: string;
+  price: Price & { price_strikethrough?: number };
+  title: string;
+  rating: Rating;
+  seller: Seller & { id: string };
+  product_id: string;
+  badge?: string;
+  variants?: Variant[];
+  general: General;
+  fulfillment: Fulfillment;
 }
 
 export interface Price {
-    price: number;
-    currency: string;
+  price: number;
+  currency: string;
 }
 
 export interface Rating {
-    count: number;
-    rating: number;
+  count: number;
+  rating: number;
 }
 
 export interface Seller {
-    name: string;
+  name: string;
 }
 
 export interface Variant {
-    url: string;
-    title: string;
-    product_id: string;
+  url: string;
+  title: string;
+  product_id: string;
+}
+
+export interface General {
+  pos: number;
+  url: string;
+  image: string;
+  title: string;
+  sponsored: boolean;
+  product_id: string;
+  out_of_stock: boolean;
+  section_title: string;
+}
+
+export interface Fulfillment {
+  pickup: boolean;
+  delivery: boolean;
+  shipping: boolean;
+  free_shipping: boolean;
 }
 
 export interface Context {
-    key: string;
-    value: any;
+  key: string;
+  value: any;
 }
 
 export interface Link {
-    rel: string;
-    href: string;
-    method: string;
+  rel: string;
+  href: string;
+  method: string;
 }
 
 export interface Job {
-    callback_url: string;
-    client_id: string;
-    context: Context[];
-    created_at: string;
-    domain: string;
-    geo_location: null | string;
-    id: string;
-    limit: number;
-    locale: null | string;
-    pages: number;
-    parse: boolean;
-    parser_type: null | string;
-    parsing_instructions: null | string;
-    browser_instructions: null | string;
-    render: null | boolean;
-    url: string;
-    query: string;
-    source: string;
-    start_page: number;
-    status: string;
-    storage_type: null | string;
-    storage_url : null | string;
-    subdomain: string;
-    content_encoding: string;
-    updated_at: string;
-    user_agent_type: string;
-    session_info: null | string;
-    statuses: any[];
-    client_notes: null | string;
-    _links: Link[];
+  callback_url: string;
+  client_id: string;
+  context: Context[];
+  created_at: string;
+  domain: string;
+  geo_location: null | string;
+  id: string;
+  limit: number;
+  locale: null | string;
+  pages: number;
+  parse: boolean;
+  parser_type: null | string;
+  parsing_instructions: null | string;
+  browser_instructions: null | string;
+  render: null | boolean;
+  url: string;
+  query: string;
+  source: string;
+  start_page: number;
+  status: string;
+  storage_type: null | string;
+  storage_url: null | string;
+  subdomain: string;
+  content_encoding: string;
+  updated_at: string;
+  user_agent_type: string;
+  session_info: null | string;
+  statuses: any[];
+  client_notes: null | string;
+  _links: Link[];
 }
